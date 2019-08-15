c3d2c10dff1f0211c7bb97a84c3c6cf27557f9f8
ktam069 <ktam069@aucklanduni.ac.nz>
2019-08-15 20:58:31 +1200

    Refactored a bit.

--- ai_project/phase2_ai.py
+++ ai_project/phase2_ai.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from keras.models import Sequential, load_model
-from keras.layers import Input, Dense, Conv2D, Flatten, BatchNormalization, Activation
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation
 # from keras.layers import Input, Dense, Conv2D, LSTM, Bidirectional
 # from keras.layers import BatchNormalization, Activation, Flatten, TimeDistributed, Reshape
 # from keras.callbacks import ModelCheckpoint
@@ -113,100 +114,42 @@ def load_data():
 	
 	return x_train, y_train, x_test, y_test, label_names
 	
-def adv_training_tut(x_train, y_train, x_test, y_test):
-	
-	if USE_LOADED_MODEL:
-		model = load_newest_model()
-	else:
-		'''Flattened features NN version'''
-		
-		# model = keras.models.Sequential()
-		# model.add(keras.layers.Flatten(input_shape=(32, 32, 3)))
-		# model.add(keras.layers.Dense(64, activation='relu'))
-		# model.add(keras.layers.Dense(32, activation='relu'))
-		# model.add(keras.layers.Dense(10, activation='softmax'))    # 10 output classes, converts to probabilities summing to 1
-		# model.summary()
-		
-		# model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-		
-		# training = model.fit(x_train, y_train, epochs=1, validation_split=0.33)
-		
-		# training.history.keys()
+def create_compiled_model():
+	'''Convolution NN model'''
 
-		# loss = training.history['loss']
-		# val_loss = training.history['val_loss']
-		# ax = pd.DataFrame(loss).plot()
-		# ax = pd.DataFrame(val_loss).plot(ax=ax)
-		# ax.legend(['loss', 'val_loss'])
-		# plt.show()
-		
-		# plt.imshow(x_test[0], cmap='gray')
-		# plt.show()
-		
-		# pred = model.predict(x_test[0:1])
-		
-		# pred = model.predict(x_test)
-		# y_pred = np.argmax(pred, axis=1)
-		# y_pred.shape
-		
-		# confusion_matrix(y_test, y_pred)
-		# sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, ax=ax)
-		
-		
-		'''CNN version'''
-		
-		# # Add extra pixels dimension (1 in this case; 3 if rgb)
-		# x_train = x_train.reshape((-1, 3072, 1))  # Shape is inferred if set as -1 (for one dim max)
-		# x_test = x_test.reshape((-1, 3072, 1))
-		
-		# Convolution NN model
-
-		model = Sequential()
-		model.add(Conv2D(8, kernel_size=(3,3), activation='relu', input_shape=(32, 32, 3)))
-		model.add(Conv2D(16, kernel_size=(3,3), activation='relu'))
-		model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
-		# model.add(Conv2D(8, kernel_size=(9,9), activation='relu'))
-		# model.add(Conv2D(32, kernel_size=(1,1), activation='relu'))
-		model.add(Flatten())
-		# model.add(Dense(20, activation='softmax'))
-		model.add(Dense(10, activation='softmax'))
-		# model.add(Dense(10, activation='softmax'))    # 10 output classes, converts to probabilities summing to 1
-		
-		model.summary()
-
-		# # Print the output shapes of each model layer
-		# for layer in model.layers:
-			# name = layer.get_config()["name"]
-			# if "batch_normal" in name or "activation" in name:
-				# continue
-			# print(layer.output_shape, "\t", name)
-		
-		model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-		training = model.fit(x_train, y_train, batch_size=1000, epochs=30, validation_split=0.3)
-		print(training.history.keys())
-		
-		loss = training.history['loss']
-		val_loss = training.history['val_loss']
-		ax = pd.DataFrame(loss).plot()
-		ax = pd.DataFrame(val_loss).plot(ax=ax)
-		ax.legend(['loss', 'val_loss'])
-		plt.show()
-		
-		save_model(model)
+	model = Sequential()
 	
-	y_pred = model.predict(x_test, verbose=1)
-	print(np.argmax(y_pred))
+	# Convolution layers with max pooling
+	model.add(Conv2D(8, kernel_size=(3,3), activation='relu', input_shape=(32, 32, 3)))
+	# model.add(MaxPooling2D(pool_size=(2,2)))
+	model.add(Conv2D(16, kernel_size=(3,3), activation='relu'))
+	# model.add(MaxPooling2D(pool_size=(2,2)))
+	model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+	# model.add(MaxPooling2D(pool_size=(2,2)))
 	
-	print(y_pred.shape)
-	y_pred = np.argmax(y_pred, axis=1)
-	print(y_pred.shape)
+	model.add(Flatten())
 	
-	plt.clf()
-	sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+	# Three fully connected layers (including the output layer)
+	# model.add(Dense(128, activation='relu'))
+	# model.add(Dense(256, activation='relu'))
+	model.add(Dense(10, activation='softmax'))    # 10 output classes, as probabilities
+	
+	model.summary()
+	
+	# (Sparse for integer encoded y_train, as opposed to one-hot encoded.)
+	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+	
+	return model
+	
+def plot_training_result(training):
+	# print(training.history.keys())
+	
+	loss = training.history['loss']
+	val_loss = training.history['val_loss']
+	ax = pd.DataFrame(loss).plot()
+	ax = pd.DataFrame(val_loss).plot(ax=ax)
+	ax.legend(['loss', 'val_loss'])
 	plt.show()
-	
-	eval_model(model, x_test, y_test)
-	
 
 def load_newest_model():
 	'''Load latest saved model'''
@@ -224,7 +167,7 @@ def load_newest_model():
 	
 	return model
 
-def save_model(model):
+def save_trained_model(model):
 	'''Save an existing (trained) model'''
 	
 	t = datetime.now().strftime("%d_%m_%H%M%S")
@@ -238,6 +181,36 @@ def eval_model(model, x_test, y_test):
 	print(model.metrics_names)
 	print("eval loss:", loss)
 	
+def display_confusion_matrix(y_test, y_pred):
+	plt.clf()
+	sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+	plt.show()
+	
+def run_CNN(x_train, y_train, x_test, y_test):
+	if USE_LOADED_MODEL:
+		model = load_newest_model()
+	else:
+		# Convolution NN model
+		model = create_compiled_model()
+
+		# TODO: hyperparameter tuning (simply using the validation result currently)
+		training = model.fit(x_train, y_train, batch_size=1000, epochs=30, validation_split=0.3)
+		
+		# Save the model so it can be loaded if desired (rather than having to re-train)
+		save_trained_model(model)
+		
+		# Plot curves of training loss and validation loss
+		plot_training_result(training)
+	
+	# Make predictions on test set
+	y_pred = model.predict(x_test, verbose=1)
+	
+	# Get the class (i.e. index) with the highest probability
+	y_pred = np.argmax(y_pred, axis=1)
+	
+	display_confusion_matrix(y_test, y_pred)
+	
+	eval_model(model, x_test, y_test)
 	
 def main():
 	print("- Program running -")
@@ -249,25 +222,7 @@ def main():
 	# x_train, y_train, x_test, y_test, label_names = load_data_batch()
 	x_train, y_train, x_test, y_test, label_names = load_data()
 	
-	
-	#=========================================
-	
-	'''Display Images...................'''
-	
-	# num_to_plot = 6
-	# for i in range(num_to_plot):
-		# plt.subplot(1,num_to_plot,i+1)
-		# plt.imshow(x_train[i], cmap='gray')
-	# plt.show()
-	
-	# plt.hist(x_train[0])
-	# plt.show()
-	
-	#=========================================
-	
-	
 	# Reshape the dataset into a shape that tensorflow expects: (samples, rows, cols, channels)
-	# Could also use (samples, channels, rows, cols)
 	# Note that the original data is stored as a flattened array in row-major order
 	
 	print(x_train.shape, '->')
@@ -286,7 +241,7 @@ def main():
 	x_train = x_train / np.max(x_train)
 	x_test = x_test / np.max(x_test)
 	
-	adv_training_tut(x_train, y_train, x_test, y_test)
+	run_CNN(x_train, y_train, x_test, y_test)
 	
 	print("- Program ended -")
 
